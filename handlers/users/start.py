1cfa2851d498d1ff31152e899cd9e42f17789176
tem12qaz <tem12qaz@yandex.ru>
2022-12-07 02:40:08 +0300

    init

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,35 @@
 from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.builtin import CommandStart, ChatTypeFilter
+from aiogram.types import ChatType
 
+from data.config import FLOOD_RATE
+from db.models import TelegramUser, Object
+from keyboards.default.keyboard import get_main_keyboard
 from loader import dp
 
 
-@dp.message_handler(CommandStart())
-async def bot_start(message: types.Message):
-    await message.answer(f'Привет, {message.from_user.full_name}!')
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), CommandStart(), state='*')
+@dp.throttled(rate=FLOOD_RATE)
+async def bot_start(message: types.Message, state: FSMContext):
+    user = await TelegramUser.get_or_none(telegram_id=message.from_user.id)
+    await state.finish()
+
+    if user is None:
+        args = message.get_args()
+        user = await TelegramUser.create(telegram_id=message.from_user.id,
+                                         username=message.from_user.username)
+
+        if 'object_' in args:
+            try:
+                object_estate = await Object.get_or_none(telegram_id=int(args.replace('object_', '')))
+            except ValueError:
+                pass
+            else:
+                await send_tour_card(user, tour, message, None)
+                return
+
+    await message.answer(
+        user.message('start_message'),
+        reply_markup=get_main_keyboard(user)
+    )
---
+++ keyboards/default/keyboard.py
@@ -0,0 +1,15 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+from db.models import TelegramUser
+
+
+def get_main_keyboard(user: TelegramUser) -> ReplyKeyboardMarkup:
+    main_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    find = KeyboardButton(user.button('find'))
+    sales = KeyboardButton(user.button('sales'))
+    profile = KeyboardButton(user.button('profile'))
+    support = KeyboardButton(user.button('support'))
+
+    main_keyboard.add(find, sales, profile, support)
+
+    return main_keyboard
