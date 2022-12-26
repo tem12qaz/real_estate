389d890f74a0da87d8a8c04e08755bb4bbfbaa26
Matvey <tem12qaz@yandex.ru>
2022-12-26 05:17:51 +0300

    fix

--- utils/google_meet.py
+++ utils/google_meet.py
@@ -115,14 +115,14 @@ def init_chrome():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    userdatadir = r'/root/code/real_estate/chrome_datad/'
+    userdatadir = r'/root/code/real_estate/chrome_data/'
     options.add_argument(f"--user-data-dir={userdatadir}")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_argument("start-maximized")
-    prefs = {"profile.managed_default_content_settings.images": 2}
-    options.add_experimental_option("prefs", prefs)
+    # options.add_argument("--disable-blink-features=AutomationControlled")
+    # options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    # options.add_experimental_option('useAutomationExtension', False)
+    # options.add_argument("start-maximized")
+    # prefs = {"profile.managed_default_content_settings.images": 2}
+    # options.add_experimental_option("prefs", prefs)
 
 
     driver = webdriver.Chrome(
