d1c2550907b60b1214607ec302e0789629fd4142
tem12qaz <tem12qaz@yandex.ru>
2022-12-30 07:26:28 +0300

    ашч

--- keyboards/inline/filter.py
+++ keyboards/inline/filter.py
@@ -131,7 +131,7 @@ def get_price_keyboard(user: TelegramUser, prices: list[str]) -> InlineKeyboardM
             ))
         ],
         [InlineKeyboardButton(text=user.button('back'),
-                              callback_data=filter_district_callback.new(_='_'))],
+                              callback_data=filter_district_callback.new(page=0))],
 
     ]
 
