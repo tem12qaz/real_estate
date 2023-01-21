17c59bdddbad9bd9cba7d68c57bd8c9901eb729f
tem12qaz <tem12qaz@yandex.ru>
2023-01-21 03:16:31 +0300

    ё

--- db/models.py
+++ db/models.py
@@ -20,7 +20,7 @@ class Developer(Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(128)
     # director = fields.OneToOneField('models.TelegramUser', related_name='developer_director')
-    manager = fields.OneToOneField('models.TelegramUser', related_name='developer_manager')
+    # manager = fields.OneToOneField('models.TelegramUser', related_name='developer_manager')
 
     chat_id = fields.CharField(32)
     photo = fields.CharField(128)
